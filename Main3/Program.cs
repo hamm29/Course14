04f0388125fabe52991703317556f1a3bb6b30f0
Hammad <hammad055hammad@gmail.com>
2025-09-26 08:03:13 +0800

    last update

---
+++ Main3/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//ProgrammingAdvices.com
+//Mohammed Abu-Hadhoud
+
+using System;
+
+namespace Main
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Nullable<int> i = null;
+		  i = 3;
+
+			var student = new { Id = 3, FName = "Hammad", LName = "Muslim" };
+
+			Console.WriteLine(student.FName);
+
+
+
+			// Console.ReadKey();
+
+		}
+	}
+}
